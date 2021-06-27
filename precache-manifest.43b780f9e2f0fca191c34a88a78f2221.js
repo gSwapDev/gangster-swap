8edfbd6c727579cc96dc1d70ceed780cd790ac9b
Roger Villalba <roger.capone@MacBook-Pro-de-Roger.local>
2021-06-27 19:48:48 +0200

    Updates

---
+++ precache-manifest.43b780f9e2f0fca191c34a88a78f2221.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c6d1da1bdf395f43fefdc89afc8706",
+    "url": "/gangster-swap/index.html"
+  },
+  {
+    "revision": "d4a2ebd435d5e4db1316",
+    "url": "/gangster-swap/static/css/2.965a2be2.chunk.css"
+  },
+  {
+    "revision": "fd9c5c8edb8243991ffb",
+    "url": "/gangster-swap/static/css/main.de7acb17.chunk.css"
+  },
+  {
+    "revision": "d4a2ebd435d5e4db1316",
+    "url": "/gangster-swap/static/js/2.7832662f.chunk.js"
+  },
+  {
+    "revision": "bb244aad54ba979e7b3eae6769f6e18b",
+    "url": "/gangster-swap/static/js/2.7832662f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9c5c8edb8243991ffb",
+    "url": "/gangster-swap/static/js/main.391b24d1.chunk.js"
+  },
+  {
+    "revision": "5eb88515decf12a13e06dfd27c183516",
+    "url": "/gangster-swap/static/js/main.391b24d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d5e8da152bfceeab65",
+    "url": "/gangster-swap/static/js/runtime-main.dac8cc66.js"
+  },
+  {
+    "revision": "2254aa3349a81c61759d4bba19e2483c",
+    "url": "/gangster-swap/static/media/ammo.2254aa33.png"
+  },
+  {
+    "revision": "d60e0d6a8eaf3a2020f5d22a5e795c66",
+    "url": "/gangster-swap/static/media/dbank.d60e0d6a.png"
+  },
+  {
+    "revision": "97c556170bddd95be5b4aa5a0b31f8c9",
+    "url": "/gangster-swap/static/media/ewt.97c55617.png"
+  },
+  {
+    "revision": "2a03f9c6d38e32be5b193879d8355d8e",
+    "url": "/gangster-swap/static/media/gang-ewt.2a03f9c6.png"
+  },
+  {
+    "revision": "cc2ed9f8e298dce1f606a314a1942a48",
+    "url": "/gangster-swap/static/media/gangster.cc2ed9f8.png"
+  },
+  {
+    "revision": "c8ec7766ab9409b0bcec5db4d371b641",
+    "url": "/gangster-swap/static/media/gngLotto.c8ec7766.png"
+  },
+  {
+    "revision": "9a4bb42721aea8482c0ed754658ef464",
+    "url": "/gangster-swap/static/media/logo.9a4bb427.png"
+  },
+  {
+    "revision": "bb5e20ec6797542dd23040786d66125b",
+    "url": "/gangster-swap/static/media/premium.bb5e20ec.png"
+  },
+  {
+    "revision": "c08648e6ec7ef632c36f66b4100bb100",
+    "url": "/gangster-swap/static/media/pugAmmo.c08648e6.png"
+  },
+  {
+    "revision": "d62aae509ff3c540d89c48dcccccf9f2",
+    "url": "/gangster-swap/static/media/pugBnb.d62aae50.png"
+  },
+  {
+    "revision": "0f533c13f0fdc96aa83af1480ad68197",
+    "url": "/gangster-swap/static/media/pugEwtt.0f533c13.png"
+  },
+  {
+    "revision": "70899a03b58dd7e57158a07f689a1405",
+    "url": "/gangster-swap/static/media/pugFather.70899a03.png"
+  },
+  {
+    "revision": "c1627066b6b594a06f3fb691867fb181",
+    "url": "/gangster-swap/static/media/pugSusu.c1627066.png"
+  },
+  {
+    "revision": "3400cbbe2349eab2752597925298e981",
+    "url": "/gangster-swap/static/media/pugUsdc.3400cbbe.png"
+  }
+]);
